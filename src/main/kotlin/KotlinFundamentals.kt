b3055a00433352ff6668b45835e4b973bb5673a6
unknown <jgguerrero@mail.fresnostate.edu>
2021-03-20 01:01:12 -0700

    lesson 40

--- src/main/kotlin/KotlinFundamentals.kt
+++ src/main/kotlin/KotlinFundamentals.kt
@@ -22,25 +22,75 @@ fun main(){
         println("it is now $roomTemp, which is $feltTemp")
     }
 
-     */
 
+    println()
+    println("for i in 1 until 10: -> ")
     for (i in 1 until 10) {    // can also use for(num in 1..10), for(i in 1 until 10)
         println("$i")
     }
 
+
+    println()
+
+    println("for num in 1..10: -> ")
     for (num in 1..10) {
         print("$num ")
     }
 
+    println()
+
     println() // line break
 
+    println("down in 10 downTo 1: -> ")
     for (down in 10 downTo 1) {
         print("$down ")
     }
 
+    println()
+
+    println()
+
+    println("steps in 10 downTo 1 step 2: -> ")
+    for (steps in 10 downTo 1 step 2) { // The step keyword will allow you to skip certain amounts of steps in the iteration
+        print("$steps ")
+    }
+    println()
+    println()
 
 
+    // TODO: Excercise - Loops
+    for (powerLevel in 1 until 10) {
+        if (powerLevel == 9) {
+            println("IT'S OVER 9000!!!")
+        } else {
+            print("$powerLevel")
+        }
 
+    }
+    var humidityLevel: Int = 80
+    var humidity: String = "Humid"
+    while (humidity == "Humid") {
+        humidityLevel -= 5
+        println("humidity has been decreased: it is now $humidityLevel")
+        if (humidityLevel <= 60) {
+            humidity = "Comfy"
+            println("the humidity level is $humidityLevel and it is $humidity")
+        }
+    }
+
+
+     */
+
+    // Functions
+
+    myFunction("This is a parameter")
+}
+
+fun myFunction(message: String) {
+    println(message)
+}
+
+fun addUp() {
 
 }
 
