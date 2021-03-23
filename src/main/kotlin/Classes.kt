3a3f23460b5db35411b2240851b8792d96f63c73
unknown <jgguerrero@mail.fresnostate.edu>
2021-03-23 03:24:56 -0700

    classes

--- src/main/kotlin/Classes.kt
+++ src/main/kotlin/Classes.kt
@@ -3,11 +3,43 @@ fun main(){
     // class - blueprint - design
     // object - implementation of blueprint
     // constructors - initializes the properties of a class when it is created
-    val person = Person("Josh", "123")
-    println(person.userName)
-    println(person.number)
-    println(person.addNumbers(5,10))
-    println(person.data)
+
+    /*
+    val user = User("Donn", "Felker")
+    user.printFullName()
+
+    println(user.fullName)
+
+    user.fullName = "Jon"
+    println(user.fullName)
+
+    user.fullName = "Jame Sparks"
+    println(user.fullName)
+
+     */
+
+//    user.age = 22
+
+//    println(user.fullNameLength)
+
+
+
+    var user = User(firstName = "Donn", lastName = "Felker", isPlatinum = true)
+//    user.printFullName()
+//    user.updateName("Bob")
+//    user.printFullName()
+//    user.printWithPrefix("Mr.")
+
+//    var friend = User(firstName = "Jane", lastName = "Doe")
+//    friend.printFullName()
+//    friend.printWithPrefix("Ms.")
+//    friend.firstNameLength()
+
+    var cousin = User("Nick")
+//    cousin.printFullName()
+
+
+
 
 
 }
